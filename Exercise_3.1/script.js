4aa242a1402d4eda52a6ad779755a3bd90afd1fc
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-27 09:45:35 +0300

    finished

--- Exercise_3.1/script.js
+++ Exercise_3.1/script.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+function checkAge() {
+  var age = document.getElementById('age').value;
+
+  // Put your code here
+  console.log('Input age: ' + age);
+
+  if (age >= 18) {
+    console.log('The user is an adult.');
+  } else if (age > 0 && age < 18) {
+    console.log('The user is not yet an adult.');
+  } else {
+    console.log('Invalid input!');
+  }
+}
