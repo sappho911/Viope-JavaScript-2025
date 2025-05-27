f11f8bd63b4be6e2ba1cc8543205f9f113ad505e
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-27 09:51:03 +0300

    Editing the content of an element

---
+++ Exercise_4.1/script.js
@@ -0,0 +1,8 @@
+function func() {
+  console.log(document.getElementById("text").innerHTML);
+
+  // Put your code here
+  document.getElementById("text").innerHTML = "JavaScript is easy and fun!";
+
+  console.log(document.getElementById("text").innerHTML);
+}
