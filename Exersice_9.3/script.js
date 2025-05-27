b83762fe494b2f3d933e65808455506cac68fb05
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-27 13:25:58 +0300

    Search

--- Exersice_9.3/script.js
+++ Exersice_9.3/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let words = 'The value of this variable is ten automatically assigned';
+
+function findTheNumber() {
+  let whiteSpace = '';
+
+  const position = words.search(/t+en/gi);
+
+  const lastIndex = words.match(/t+en/gi).join('').length;
+
+  let part = words.substring(position, position + lastIndex).toLowerCase();
+
+  for (let i = 0; i < position; i++) {
+    whiteSpace = whiteSpace + ' ';
+  }
+
+  const output = whiteSpace + part;
+  console.log(words);
+  console.log(output);
+}
+
+findTheNumber();
