85fcf51a7ef7f2589ebd01e2c95b678e821945bc
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-27 09:53:50 +0300

    Calling a function from the HTML page

---
+++ Exercise_4.2/script.js
@@ -0,0 +1,7 @@
+function calcSquare() {
+  const number = document.getElementById('number').innerHTML;
+
+  const result = number * number;
+
+  console.log(`The square of ${number} is ${result}`);
+}
