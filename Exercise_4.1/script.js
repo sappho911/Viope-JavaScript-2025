fb1f4a815d69d6757f4e60ab1710f33fc133e235
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-27 09:51:38 +0300

    Editing the content of an element

--- Exercise_4.1/script.js
+++ Exercise_4.1/script.js
@@ -1,8 +1,8 @@
 function func() {
-  console.log(document.getElementById("text").innerHTML);
+  console.log(document.getElementById('text').innerHTML);
 
   // Put your code here
-  document.getElementById("text").innerHTML = "JavaScript is easy and fun!";
+  document.getElementById('text').innerHTML = 'JavaScript is easy and fun!';
 
-  console.log(document.getElementById("text").innerHTML);
+  console.log(document.getElementById('text').innerHTML);
 }
