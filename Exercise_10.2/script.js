76dadc2704f14ed57000f89b78194678fdb9a4bf
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-29 16:02:11 +0300

    Customer registry

---
+++ Exercise_10.2/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// // IIFE function
+// (function () {
+//   // statements…
+// })();
+
+// Put your code here
+
+shop.customerRegistry.customerInfo(0);
+shop.customerRegistry.add('Pete', 'Programmer');
+shop.customerRegistry.add('Cara', 'Coder');
+shop.customerRegistry.customerList(1);
+shop.customerRegistry.add('Samuel', 'Scripter');
+shop.customerRegistry.add('Wendy', 'Webmaster');
+shop.customerRegistry.customerList();
