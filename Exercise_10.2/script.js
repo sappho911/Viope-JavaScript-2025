869d494839a7ba36a1db1915e77d008546b8620f
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-30 19:52:11 +0300

    Inheritance using class

--- Exercise_10.2/script.js
+++ Exercise_10.2/script.js
@@ -7,10 +7,44 @@
 
 // Put your code here
 
-shop.customerRegistry.customerInfo(0);
-shop.customerRegistry.add('Pete', 'Programmer');
-shop.customerRegistry.add('Cara', 'Coder');
-shop.customerRegistry.customerList(1);
-shop.customerRegistry.add('Samuel', 'Scripter');
-shop.customerRegistry.add('Wendy', 'Webmaster');
-shop.customerRegistry.customerList();
+// shop.customerRegistry.customerInfo(0);
+// shop.customerRegistry.add('Pete', 'Programmer');
+// shop.customerRegistry.add('Cara', 'Coder');
+// shop.customerRegistry.customerList(1);
+// shop.customerRegistry.add('Samuel', 'Scripter');
+// shop.customerRegistry.add('Wendy', 'Webmaster');
+// shop.customerRegistry.customerList();
+
+//  EXAMPLE
+var store = {};
+
+store.cart = (function () {
+  var products = [];
+  function addProduct(productName) {
+    if (!products[productName]) {
+      // If the product is not added into the shopping cart, add it there.
+      products[productName] = 0;
+    }
+    // Raise the number of products by one
+    products[productName]++;
+  }
+
+  function totalProducts() {
+    var amount = 0;
+    for (var productName in products) {
+      amount += products[productName];
+    }
+    return amount;
+  }
+
+  // interface
+  return {
+    add: addProduct,
+    productAmount: totalProducts,
+  };
+})();
+
+store.cart.add('keksi');
+store.cart.add('keksi');
+store.cart.add('apple');
+console.log(store.cart.productAmount()); // 3
--- Exercise_11.3/script.js
+++ Exercise_11.3/script.js
@@ -11,3 +11,16 @@ class Shape {
     this.y = y;
   }
 }
+
+class Circle extends Shape {
+  constructor(x, y, radius) {
+    super(x, y), (this.radius = radius);
+  }
+
+  getArea() {
+    return 3.14159 * this.radius * this.radius;
+  }
+}
+
+const circle = new Circle(0, 0, 2);
+console.log(circle.getArea());
