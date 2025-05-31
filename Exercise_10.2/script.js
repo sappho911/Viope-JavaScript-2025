dcabdce1dc337e8817a4ef22498839d73c5de4f3
Aleksandr Kuzmich <blademaster852@hotmail.com>
2025-05-31 15:00:27 +0300

    Customer registry

--- Exercise_10.2/script.js
+++ Exercise_10.2/script.js
@@ -5,46 +5,38 @@
 //   // statements…
 // })();
 
-// Put your code here
+const shop = {};
 
-// shop.customerRegistry.customerInfo(0);
-// shop.customerRegistry.add('Pete', 'Programmer');
-// shop.customerRegistry.add('Cara', 'Coder');
-// shop.customerRegistry.customerList(1);
-// shop.customerRegistry.add('Samuel', 'Scripter');
-// shop.customerRegistry.add('Wendy', 'Webmaster');
-// shop.customerRegistry.customerList();
+shop.customerRegistry = (function () {
+  let users = [];
 
-//  EXAMPLE
-var store = {};
-
-store.cart = (function () {
-  var products = [];
-  function addProduct(productName) {
-    if (!products[productName]) {
-      // If the product is not added into the shopping cart, add it there.
-      products[productName] = 0;
-    }
-    // Raise the number of products by one
-    products[productName]++;
+  function add(firstname, lastname) {
+    users.push({ firstname: firstname, lastname: lastname });
+    console.log(`Added customer with ID  ${users.length - 1}`);
   }
 
-  function totalProducts() {
-    var amount = 0;
-    for (var productName in products) {
-      amount += products[productName];
-    }
-    return amount;
+  function customerInfo(id) {
+    const userID = users.length;
+    id < 0 || id >= userID ? console.log('Too high an ID value!') : '';
+  }
+  function customerList() {
+    console.log('Listing all customers:');
+    users.forEach(function (user, i) {
+      console.log(i + ': ' + user.lastname + ' ' + user.firstname);
+    });
   }
 
-  // interface
   return {
-    add: addProduct,
-    productAmount: totalProducts,
+    add: add,
+    customerInfo: customerInfo,
+    customerList: customerList,
   };
 })();
 
-store.cart.add('keksi');
-store.cart.add('keksi');
-store.cart.add('apple');
-console.log(store.cart.productAmount()); // 3
+shop.customerRegistry.customerInfo(0);
+shop.customerRegistry.add('Pete', 'Programmer');
+shop.customerRegistry.add('Cara', 'Coder');
+shop.customerRegistry.customerList(1);
+shop.customerRegistry.add('Samuel', 'Scripter');
+shop.customerRegistry.add('Wendy', 'Webmaster');
+shop.customerRegistry.customerList();
